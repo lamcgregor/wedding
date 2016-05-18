b66a53df62ce85a0512c8024c79869907fee349a
Frank Hamand <frankhamand@gmail.com>
2016-05-18 20:40:56 +0100

    Fuck title case

--- api/wedding_api/rsvp/forms.py
+++ api/wedding_api/rsvp/forms.py
@@ -4,14 +4,14 @@ from django.forms import formset_factory
 from .models import Guest
 
 class GuestForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=255, required=True)
-    last_name = forms.CharField(label='Last Name', max_length=255, required=True)
+    first_name = forms.CharField(label='First name', max_length=255, required=True)
+    last_name = forms.CharField(label='Last name', max_length=255, required=True)
 
 class RsvpForm(forms.Form):
-    attending = forms.ChoiceField(choices=[('', 'Please Choose...'), ('yes', 'Yes'), ('no', 'No')], required=False)
-    email = forms.EmailField(label='Email Address', required=False)
-    dietary_requirements = forms.ChoiceField(choices=[('', 'No Special Requirements'), ('vegetarian', 'Vegetarian'), ('vegan', 'Vegan'), ('other', 'Other')], required=False)
-    dietary_other = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Please Specify'}), required=False)
+    attending = forms.ChoiceField(choices=[('', 'Please choose...'), ('yes', 'Yes'), ('no', 'No')], required=False)
+    email = forms.EmailField(label='Email address', required=False)
+    dietary_requirements = forms.ChoiceField(choices=[('', 'No special requirements'), ('vegetarian', 'Vegetarian'), ('vegan', 'Vegan'), ('other', 'Other')], required=False)
+    dietary_other = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Please specify'}), required=False)
     comments = forms.CharField(label='Comments', required=False)
     guest = forms.ModelChoiceField(queryset=Guest.objects.none(), label='Test', widget=forms.HiddenInput())
 
