388020b5e3d90fd9c0f90e42e4070f02064b3c9d
Frank Hamand <frankhamand@gmail.com>
2016-05-20 14:48:55 +0100

    Update production settings

--- api/wedding_api/wedding_api/settings/development.py
+++ api/wedding_api/wedding_api/settings/development.py
@@ -1,6 +1,12 @@
 import importlib
 globals().update(importlib.import_module('.shared', package=__package__).__dict__)
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$%cnuj4v*6bm4zjbkgi)v&hmpr*+c6td4ud(&z+9p1d(&%$oua'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
--- api/wedding_api/wedding_api/settings/production.py
+++ api/wedding_api/wedding_api/settings/production.py
@@ -1,5 +1,60 @@
 import importlib
 globals().update(importlib.import_module('.shared', package=__package__).__dict__)
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = False
+ALLOWED_HOSTS = ['lukeandpeggy.com']
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
+        },
+    },
+]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'WARNING',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
@@ -10,3 +65,5 @@ DATABASES = {
         'NAME': '/data/db.sqlite3',
     }
 }
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
