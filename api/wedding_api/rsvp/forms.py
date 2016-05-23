f737dc3627d51ee729b9cea5f77c7b9df6db19f9
Luke Woollett <luke@aqueduct.co.uk>
2016-05-23 17:06:43 +0100

    Form changes and last minute content changes

--- api/wedding_api/rsvp/forms.py
+++ api/wedding_api/rsvp/forms.py
@@ -8,16 +8,17 @@ class GuestForm(forms.Form):
     last_name = forms.CharField(label='Last name', max_length=255, required=True)
 
 class RsvpForm(forms.Form):
-    attending = forms.ChoiceField(choices=[('', 'Please choose...'), ('yes', 'Yes'), ('no', 'No')], required=False)
+    attending = forms.ChoiceField(choices=[('', 'Please choose...'), ('yes', 'Yes'), ('no', 'No')], required=True)
     email = forms.EmailField(label='Email address', required=False)
     dietary_requirements = forms.ChoiceField(
         choices=[
-            ('', 'No special requirements'), 
+            ('none', 'No special requirements'),
             ('vegetarian', 'Vegetarian'), 
-            ('vegan', 'Vegan'), 
+            ('vegan', 'Vegan'),
+            ('nopork', 'No pork'),
             ('other', 'Other')
         ], 
-        required=False,
+        required=True,
         widget=forms.Select(attrs={'class': 'dietary-requirements-field'}))
 
     dietary_other = forms.CharField(label='', widget=forms.Textarea(
@@ -27,7 +28,12 @@ class RsvpForm(forms.Form):
             'rows': 5}
         ), 
         required=False)
-    comments = forms.CharField(label='Comments', required=False)
+    comments = forms.CharField(label='Comments',  widget=forms.Textarea(
+        attrs={
+            'placeholder': 'Please leave any messages here',
+            'rows': 1}
+        ),
+        required=False)
     guest = forms.ModelChoiceField(queryset=Guest.objects.none(), label='Test', widget=forms.HiddenInput())
 
 RsvpFormSet = formset_factory(RsvpForm, extra=0)
